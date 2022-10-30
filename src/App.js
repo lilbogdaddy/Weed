bd9b4ad7a32706766abcbbc204450d8f74ffd212
lilbogdaddy <lilbogdaddy@users.noreply.github.com>
2022-10-30 15:14:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from 'react'
+import { Canvas } from '@react-three/fiber';
+import "./App.css";
+import { OrbitControls } from "@react-three/drei";
+import Model from './Anothernew'
+import Design1 from "./Design1";
 
-function App() {
+export default function App() {
+
+  const [currentModel, setCurrentModel] = useState(<Model />);
+
+  const handleClick = (e) => {
+    let value = e.target.value
+    if(value === 'Model'){
+      setCurrentModel(<Model />)
+    }
+    if(value === 'Design1'){
+      setCurrentModel(<Design1 />)
+    }
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="canvas">
+        <Canvas camera={{position: [0, 0, 50]}}>
+          {currentModel}
+          <hemisphereLight args={['#fff', '#333', 0.5]} />
+          <ambientLight intensity={0.1} />
+          {/* <ambientLight args={[0xff0000]} intensity={0} />
+          <directionalLight position={[0, 0, 20]} intensity={0.5} /> */}
+          <OrbitControls target={[0, 0, 0]}/>
+        </Canvas>
+      </div>
+
+      <div className="buttons">
+          <button className="button" value='Model' onClick={handleClick}>1</button>
+          <button className="button" value="Design1" onClick={handleClick}>2</button>
+      </div>
     </div>
   );
 }
-
-export default App;
